ec1c450dcd89469115d0402050ac43db4778d234
Yu-Jhen Hsu <rovenr32@gmail.com>
2023-10-31 09:26:02 +0000

    Implement Level 2

---
+++ Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+    [SerializeField] private Score score;
+    [SerializeField] private Sprite[] backgrounds;
+    [SerializeField] private int threshold = 10;
+
+    private SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = backgrounds[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int level = score.GetScore() / threshold;
+        if (level > backgrounds.Length)
+        {
+            spriteRenderer.sprite = backgrounds[^1];
+        }
+        else
+        {
+            spriteRenderer.sprite = backgrounds[level];
+        }
+    }
+}
